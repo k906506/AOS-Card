c62884bb774e9f2bc9946627845af2a1ba14636b
k906506 <k906506@naver.com>
2021-11-03 23:33:06 +0900

    로그인, 로그아웃 상태에 따라 다른 화면 출력

--- app/src/main/java/com/example/tinder/MainActivity.kt
+++ app/src/main/java/com/example/tinder/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.example.tinder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    private val button: Button by lazy {
-        findViewById()
+    override fun onStart() {
+        super.onStart()
+
+        if (auth.currentUser == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+        } else {
+            startActivity(Intent(this, LikeActivity::class.java))
+            finish()
+
+        }
     }
 }
